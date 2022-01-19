946f88a0e813d8e1e270e909700c92d88386ac22
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-01-19 12:57:09 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'pages/api'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+
 }
