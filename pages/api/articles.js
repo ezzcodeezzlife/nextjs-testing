f318dc9175b8962108dd18e6e6e6e3926720b320
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-01-19 12:21:07 +0100

    test

---
+++ pages/api/articles.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+
+const article = 
+    {
+    title: "Mien title 1",
+    text: "mytext",
+    author: "nibba",
+    date: "212133",
+    }
+export default function handler(req, res) {
+    res.status(200).json(article)
+  }
+  
