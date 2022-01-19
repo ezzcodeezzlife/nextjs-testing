49ffb17713c1f611598eb5a1fb9b9a4ef6d9492b
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-01-19 13:58:44 +0100

    more testing

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -1,15 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const article = {
+    title: 'Mien title 1',
+    text: 'mytext',
+    author: 'nibba',
+    date: '212133',
+}
 
-const article = 
-    {
-    title: "Mien title 1",
-    text: "mytext",
-    author: "nibba",
-    date: "212133",
-    }
-    
 export default function handler(req, res) {
     res.status(200).json(JSON.stringify(article))
-  }
-  
+}
